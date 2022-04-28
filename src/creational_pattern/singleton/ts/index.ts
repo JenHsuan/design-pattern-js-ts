cbb6e90187603aa0e6963c637aa56ffa17134990
Sean Hsieh <hsiehs@fortinet.com>
2022-04-28 15:56:19 +0800

    Add the singleton pattern

---
+++ src/creational_pattern/singleton/ts/index.ts
@@ -0,0 +1,36 @@
+class Singleton {
+    static instance: Singleton;
+    #table: { [id: number]: string } = {};
+
+    constructor() {
+        if (Singleton.instance) {
+            return Singleton.instance;
+        }
+        Singleton.instance = this;
+    }
+
+    //Test the data will be persist
+    addData(id: number, description: string): void {
+        this.#table[id] = description;
+    }
+
+    print(): void {
+        for (const key in this.#table) {
+            console.log(`|\t${key}\t|\t${this.#table[key]}\t|`);
+        }
+    }
+}
+
+const singleton1 = new Singleton();
+singleton1.addData(1, 'member 1');
+singleton1.addData(2, 'member 2');
+singleton1.addData(3, 'member 3');
+
+
+const singleton2 = new Singleton();
+singleton2.print();
+/* will print:
+ * |       1       |       member 1        |
+ * |       2       |       member 2        |
+ * |       3       |       member 3        |
+ */
